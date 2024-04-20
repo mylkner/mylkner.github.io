f14543f17daa45f9fd6e384403d0b14fb1f74785
mylkner <mylofaulkner27@gmail.com>
2024-04-20 19:33:00 +0200

    added skillCard component

---
+++ src/Components/SkillCard.js
@@ -0,0 +1,9 @@
+const SkillCard = (props) => {
+    return (
+        <div className="skillCard">
+            <p className="skillName">{props.skillName}</p>
+        </div>
+    );
+};
+
+export default SkillCard;
