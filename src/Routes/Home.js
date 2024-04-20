6dec8ef6e5c3d6881412adbdf44bec069d4040f5
mylkner <mylofaulkner27@gmail.com>
2024-04-20 19:33:50 +0200

    added home page

---
+++ src/Routes/Home.js
@@ -0,0 +1,17 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="homeInfo">
+                <h1 className="homeTitle">Name</h1>
+                <article className="aboutMe">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Explicabo quasi assumenda numquam culpa tenetur at nobis
+                    sunt illum laborum qui doloribus quam delectus eligendi,
+                    harum earum et nihil, rerum unde.
+                </article>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
