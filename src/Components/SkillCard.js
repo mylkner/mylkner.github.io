dd27d6072b28e90f17747c548f3a0ff81dfbea86
mylkner <mylofaulkner27@gmail.com>
2024-04-22 10:40:44 +0200

    created skill card

--- src/Components/SkillCard.js
+++ src/Components/SkillCard.js
@@ -1,8 +1,21 @@
+import { motion } from "framer-motion";
+
 const SkillCard = (props) => {
+    const skillVariants = {
+        initial: { opacity: 0, y: -30 },
+        final: { opacity: 1, y: 0 },
+    };
+
     return (
-        <div className="skillCard">
+        <motion.div
+            className="skillCard"
+            variants={skillVariants}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ duration: 0.2 }}
+        >
             <p className="skillName">{props.skillName}</p>
-        </div>
+        </motion.div>
     );
 };
 
