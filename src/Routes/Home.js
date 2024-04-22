9057a7e4baeb97dc1873e257d7c04002656004a9
mylkner <mylofaulkner27@gmail.com>
2024-04-22 10:41:42 +0200

    added home page

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,14 +1,51 @@
+import { motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import aboutMeText from "../Assets/aboutMeText";
+
 const Home = () => {
+    const [text, setText] = useState("");
+    const count = useRef(1);
+
+    useEffect(() => {
+        if (count.current <= aboutMeText.length) {
+            const timer = setTimeout(() => {
+                setText(aboutMeText.slice(0, count.current));
+                count.current += 1;
+            }, 20);
+
+            return () => clearTimeout(timer);
+        }
+    });
+
+    const titleVariants = {
+        initial: { opacity: 0 },
+        final: { opacity: 1, transition: { duration: 1 } },
+    };
+
+    const articleVariants = {
+        initial: { opacity: 0 },
+        final: { opacity: 1, transition: { delay: 0.1, duration: 0.5 } },
+    };
+
     return (
         <div className="home">
             <div className="homeInfo">
-                <h1 className="homeTitle">Name</h1>
-                <article className="aboutMe">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Explicabo quasi assumenda numquam culpa tenetur at nobis
-                    sunt illum laborum qui doloribus quam delectus eligendi,
-                    harum earum et nihil, rerum unde.
-                </article>
+                <motion.h1
+                    className="homeTitle"
+                    variants={titleVariants}
+                    initial="initial"
+                    animate="final"
+                >
+                    About Me
+                </motion.h1>
+                <motion.article
+                    className="homeAbout"
+                    variants={articleVariants}
+                    initial="initial"
+                    animate="final"
+                >
+                    {text}
+                </motion.article>
             </div>
         </div>
     );
