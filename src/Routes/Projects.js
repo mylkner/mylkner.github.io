5e438ba9968a27a17e13a4858a0e12a7e837dd75
mylkner <mylofaulkner27@gmail.com>
2024-04-22 19:50:03 +0200

    added the projects

--- src/Routes/Projects.js
+++ src/Routes/Projects.js
@@ -1,9 +1,53 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+import {
+    javaScriptProjectData,
+    reactProjectData,
+    d3ProjectData,
+} from "../Assets/projectData.js";
+import ProjectCarousel from "../Components/ProjectComponents/ProjectCarousel.js";
+import ProjectDropown from "../Components/ProjectComponents/ProjectDropown.js";
+
 const Projects = () => {
+    const [projectCategory, setProjectCategory] = useState("JavaScript");
+
+    const projectDataMap = {
+        JavaScript: javaScriptProjectData,
+        React: reactProjectData,
+        D3: d3ProjectData,
+    };
+
+    const variants = {
+        initial: { opacity: 0 },
+        final: { opacity: 1, transition: { delay: 0.5, duration: 0.7 } },
+    };
+
+    const handleClick = (category) => {
+        setProjectCategory(category);
+    };
+
     return (
         <div className="projects">
-            <div className="projectsTitleContainer">
+            <motion.div
+                className="projectsTitleContainer"
+                variants={variants}
+                initial="initial"
+                animate="final"
+            >
                 <h1 className="projectsTitle">Projects</h1>
-            </div>
+                <span className="projectCategoryDropdown">
+                    <h2 className="projectCategory">{projectCategory}</h2>
+                    <ProjectDropown
+                        onClick={handleClick}
+                        currentCategory={projectCategory}
+                    />
+                </span>
+            </motion.div>
+
+            <ProjectCarousel
+                projectData={projectDataMap[projectCategory]}
+                variants={variants}
+            />
         </div>
     );
 };
