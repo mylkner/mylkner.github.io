150982ae729cb6e9a16c860c7ea29daeeed1a6f0
mylkner <mylofaulkner27@gmail.com>
2024-04-23 11:35:40 +0200

    added key to reset animation on screen resize

--- src/Components/BallAnimation.js
+++ src/Components/BallAnimation.js
@@ -1,19 +1,29 @@
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
+import useScreenWidth from "../Hooks/useScreenWidth";
 
 const BallAnimation = () => {
+    const screenWidth = useScreenWidth();
+    const [aniKey, setAniKey] = useState(0);
+
+    useEffect(() => {
+        setAniKey((anikey) => (anikey ? 0 : 1));
+    }, [screenWidth]);
+
     return (
         <motion.div
+            key={aniKey}
             initial={{ opacity: 0, x: -100 }}
-            animate={{ opacity: 1, x: window.innerWidth + 100 }}
+            animate={{ opacity: 1, x: screenWidth + 100 }}
             transition={{
-                duration: Math.random() * 4 + 1,
+                duration: Math.random() * 1.8 + 1,
                 delay: 0.5,
                 ease: "easeIn",
                 repeat: Infinity,
             }}
             style={{
                 position: "absolute",
-                top: Math.random() * 100 + 530,
+                top: Math.random() * 80,
                 backgroundColor: Math.random() >= 0.5 ? "#ff94d5" : "#e20071",
                 width: "10px",
                 height: "10px",
