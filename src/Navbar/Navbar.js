470172e07b6d6bbc7268444f3f6409e28b56886d
mylkner <mylofaulkner27@gmail.com>
2024-04-23 11:16:48 +0200

    replaced getting screen width logic with hook

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,22 +1,10 @@
-import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import useScreenWidth from "../Hooks/useScreenWidth.js";
 import Dropdown from "./Dropdown.js";
 import Links from "./Links.js";
 
 const Navbar = () => {
-    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setScreenWidth(window.innerWidth);
-        };
-
-        window.addEventListener("resize", handleResize);
-
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    });
+    const screenWidth = useScreenWidth();
 
     return (
         <>
