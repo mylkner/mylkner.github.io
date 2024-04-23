9397b30e6f9a118cd0b9e55dc95f7c4c8f6d89e9
mylkner <mylofaulkner27@gmail.com>
2024-04-23 15:37:28 +0200

    updated animation

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -18,11 +18,11 @@ const Home = () => {
     });
 
     const titleVariants = {
-        initial: { opacity: 0, x: -20 },
+        initial: { opacity: 0, x: -100 },
         final: {
             opacity: 1,
             x: 0,
-            transition: { type: "spring", duration: 0.5 },
+            transition: { type: "spring", duration: 0.7 },
         },
     };
 
