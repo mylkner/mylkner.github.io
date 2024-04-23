061b7261492200d8fcae2decca65a9873949c5e4
mylkner <mylofaulkner27@gmail.com>
2024-04-23 11:16:58 +0200

    decreased amount of balls

--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -83,7 +83,7 @@ const Contact = () => {
             </motion.div>
 
             <div className="ballAniContainer">
-                {new Array(50).fill(0).map((_, i) => (
+                {new Array(30).fill(0).map((_, i) => (
                     <BallAnimation key={i} />
                 ))}
             </div>
