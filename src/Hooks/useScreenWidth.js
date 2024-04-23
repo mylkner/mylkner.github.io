2c44269997aae5266688be5ddf14d77b877bd36c
mylkner <mylofaulkner27@gmail.com>
2024-04-23 11:16:11 +0200

    added screenWidth hook

---
+++ src/Hooks/useScreenWidth.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+const useScreenWidth = () => {
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenWidth(window.innerWidth);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    });
+
+    return screenWidth;
+};
+
+export default useScreenWidth;
