570e91e462328cdb271365441343a8691de1935a
Mylo Faulkner <mylofaulkner27@gmail.com>
2024-12-08 22:33:54 +0000

    Update aboutMeText.js
    updated text to placeholder

--- src/Assets/aboutMeText.js
+++ src/Assets/aboutMeText.js
@@ -1,4 +1,4 @@
 const aboutMeText =
-    "Self-taught developer from Ireland with an interest in computer science. I've been passionate about coding for a while now. Have mostly done frontend development but also do backend. When I'm not coding I like to read or pursue piano as a hobby.";
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dolor eros, auctor vitae mauris id, mollis aliquet enim. Vivamus porttitor vestibulum nisi, vitae ornare diam convallis eget. Ut est nibh, lobortis et lorem non, tristique condimentum lorem. Etiam malesuada orci non semper imperdiet. Maecenas placerat, ligula ut vestibulum volutpat, massa lacus efficitur dolor, in viverra risus enim in justo. Nullam sed sollicitudin justo. Duis id felis dolor. Nam ante est, aliquet non auctor et, mollis a velit. Ut sit amet est mattis, fringilla leo quis, iaculis magna. Phasellus quis eleifend eros.";
 
 export default aboutMeText;
